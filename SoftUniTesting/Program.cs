621964f587036ae0f3cb8513f1757ef552e8e9a6
Jin <Jin@JIN-ROG>
2017-01-08 21:54:33 +0200

    Cristmas Tree

--- SoftUniTesting/Program.cs
+++ SoftUniTesting/Program.cs
@@ -8,8 +8,20 @@ namespace SoftUniTesting
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; ++i)
+            {
+                string str1 = new string('*', i);
+                string str2 = new string(' ', i);
+                Console.Write(str2);
+                Console.Write(str1);
+                Console.Write(" | ");
+                Console.Write(str1);
+                Console.WriteLine();
+            }
+
         }
     }
 }
